4da9b525e9e0a9f19acabff798af46e82fc873fc
CassJB <100716237+CassJB@users.noreply.github.com>
2022-04-07 14:59:40 -0500

    Update CassJB_project2_code.ino
    if button is pressed, turn servo

--- CassJB_project2_code/CassJB_project2_code.ino
+++ CassJB_project2_code/CassJB_project2_code.ino
@@ -10,14 +10,22 @@
 
 //VARIABLES
 Servo servo1;
-const int servo1Pin = 7;
+const int buttonPin = 7;
 
 void setup() {
   // put your setup code here, to run once:
-
+  servo1.attatch(9);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  //if button pressed, move servo
+  if(digitalRead(buttonPin) == HIGH)
+  {
+    servo1.write(180);
+    }
+   else
+   {
+    servo1.write(0);
+    }
 }
