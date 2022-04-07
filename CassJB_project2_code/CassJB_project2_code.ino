3d3cf166630084b61b59c2f39fb09f048cf4df98
CassJB <100716237+CassJB@users.noreply.github.com>
2022-04-07 14:53:13 -0500

    Create CassJB_project2_code.ino
    created variables & initialized servo/button pins

---
+++ CassJB_project2_code/CassJB_project2_code.ino
@@ -0,0 +1,23 @@
+/*
+ * AET Apollo & Daphne Enclosure
+ * Coderella
+ * (authors)
+ * (desc)
+ */
+
+//servo code library
+#include<Servo.h>;
+
+//VARIABLES
+Servo servo1;
+const int servo1Pin = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
